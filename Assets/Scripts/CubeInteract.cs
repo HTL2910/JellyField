cd42db4c62bf461a23dfe39871b699c4eeb24eb6
HTL2910 <hathanhlong2910@gmail.com>
2024-10-04 12:23:34 +0700

    lasted code update, temp feature for rescale object of group

--- Assets/Scripts/CubeInteract.cs
+++ Assets/Scripts/CubeInteract.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -6,13 +6,89 @@ using UnityEngine;
 public class CubeInteract : MonoBehaviour
 {
     public bool isMove ;
+    private List<GameObject> list = new List<GameObject>();
     private void OnTriggerStay(Collider other)
     {
 
-        if (other.gameObject.CompareTag(this.gameObject.tag) == true && isMove==false && other.gameObject.GetComponent<CubeInteract>().isMove==false)
+        if (other.gameObject.CompareTag(this.gameObject.tag) == true && isMove == false && other.gameObject.GetComponent<CubeInteract>().isMove == false)
         {
+            GameObject parent;
+            GameObject otherParent;
+            if (other.transform.parent != null)
+            {
+                otherParent = other.transform.parent.gameObject;
+                if (otherParent.transform.childCount == 2)
+                {
+                    Vector3 center;
+                    for(int i = 0; i < otherParent.transform.childCount; i++)
+                    {
+                        if (otherParent.transform.GetChild(i).gameObject != other.gameObject)
+                        {
+                            GameObject tmpObj = otherParent.transform.GetChild(i).gameObject;
+                            center =GetCenterPosition(other.gameObject, tmpObj);
+                            tmpObj.transform.position=center;
+                            if (otherParent.transform.childCount == 1)
+                            {
+                                StartCoroutine(Destroytrigger(otherParent.gameObject, this.gameObject));
 
-            StartCoroutine(Destroytrigger(other.gameObject, this.gameObject));
+                            }
+                            
+                            if (tmpObj.transform.localScale.x <0.9f)
+                            {
+                                Vector3 newScale = tmpObj.transform.localScale;
+                                newScale.x *= 2; // Tăng kích thước x gấp đôi
+                                tmpObj.transform.localScale = newScale;
+                            }
+                            else if (tmpObj.transform.localScale.y <0.9f)
+                            {
+                                Vector3 newScale = tmpObj.transform.localScale;
+                                newScale.y *= 2; // Tăng kích thước y gấp đôi
+                                tmpObj.transform.localScale = newScale;
+                            }
+                        }
+                    }
+                }
+                
+            }
+            else if(gameObject.transform.parent!=null)
+            {
+                parent=gameObject.transform.parent.gameObject;
+                Vector3 center;
+                for (int i = 0; i < parent.transform.childCount; i++)
+                {
+                    if (parent.transform.GetChild(i).gameObject != gameObject)
+                    {
+                        GameObject tmpObj = parent.transform.GetChild(i).gameObject;
+                        center = GetCenterPosition(gameObject, tmpObj);
+                        tmpObj.transform.position = center;
+                        if (parent.transform.childCount == 1)
+                        {
+                            StartCoroutine(Destroytrigger(other.gameObject, parent.gameObject));
+
+                        }
+                     
+                        if (tmpObj.transform.localScale.x <0.9f)
+                        {
+                            Vector3 newScale = tmpObj.transform.localScale;
+                            newScale.x *= 2; // Tăng kích thước x gấp đôi
+                            tmpObj.transform.localScale = newScale;
+                        }
+                        else if (tmpObj.transform.localScale.y < 0.9f)
+                        {
+                            Vector3 newScale = tmpObj.transform.localScale;
+                            newScale.y *= 2; // Tăng kích thước y gấp đôi
+                            tmpObj.transform.localScale = newScale;
+                        }
+                    }
+                }
+            }
+            if(other.gameObject!=null && this.gameObject != null)
+            {
+                StartCoroutine(Destroytrigger(other.gameObject, this.gameObject));
+
+            }
+
+
         }
     }
     IEnumerator Destroytrigger(GameObject obj1, GameObject obj2)
@@ -21,5 +97,14 @@ public class CubeInteract : MonoBehaviour
         Destroy(obj1);
         Destroy(obj2);
     }
-  
+    private Vector3 GetCenterPosition(GameObject obj1, GameObject obj2)
+    {
+        Vector3 position1 = obj1.transform.position;
+        Vector3 position2 = obj2.transform.position;
+
+        Vector3 centerPosition = (position1 + position2) / 2.0f;
+
+        return centerPosition;
+    }
+
 }
--- Assets/Scripts/ResizeCubeChild.cs
+++ Assets/Scripts/ResizeCubeChild.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class ResizeCubeChild : MonoBehaviour
 {
-    public List<GameObject> listCurrentChild;
-    public List<GameObject> listNewChild;
+  
 
     private void Start()
     {
-        listCurrentChild = AddList();
+        InvokeRepeating("DestroyInvoke", 1f, 3f);
     }
-
+    void DestroyInvoke()
+    {
+        if (gameObject.transform.childCount <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
     private List<GameObject> AddList()
     {
         List<GameObject> list = new List<GameObject>();
@@ -22,57 +27,57 @@ public class ResizeCubeChild : MonoBehaviour
         return list;
     }
 
-    private void Update()
-    {
-        listNewChild = AddList();
-        ResizeChild();
-        // Cập nhật lại listCurrentChild nếu có sự thay đổi
-        if (listCurrentChild.Count != listNewChild.Count)
-        {
-            listCurrentChild = new List<GameObject>(listNewChild);
-        }
-    }
+    //private void Update()
+    //{
+    //    listNewChild = AddList();
+    //    ResizeChild();
+    //    // Cập nhật lại listCurrentChild nếu có sự thay đổi
+    //    if (listCurrentChild.Count != listNewChild.Count)
+    //    {
+    //        listCurrentChild = new List<GameObject>(listNewChild);
+    //    }
+    //}
 
-    private void ResizeChild()
-    {
-        int count = transform.childCount;
-        if (count < listCurrentChild.Count)
-        {
-            int index = CheckIndex();
-            if (index >= 0 && index < listCurrentChild.Count)
-            {
-                // Lấy child tương ứng và kiểm tra index
-                Transform childToResize = null;
-                if (index % 2 == 0)
-                {
-                    childToResize = transform.GetChild(index + 1);
-                }
-                else
-                {
-                    childToResize = transform.GetChild(index - 1);
+    //private void ResizeChild()
+    //{
+    //    int count = transform.childCount;
+    //    if (count < listCurrentChild.Count)
+    //    {
+    //        int index = CheckIndex();
+    //        if (index >= 0 && index < listCurrentChild.Count)
+    //        {
+    //            // Lấy child tương ứng và kiểm tra index
+    //            Transform childToResize = null;
+    //            if (index % 2 == 0)
+    //            {
+    //                childToResize = transform.GetChild(index + 1);
+    //            }
+    //            else
+    //            {
+    //                childToResize = transform.GetChild(index - 1);
 
-                }
+    //            }
                
-                if (childToResize != null) // Nếu child không null
-                {
-                    Vector3 newScale = childToResize.localScale;
-                    newScale.y *= 2; // Tăng kích thước y gấp đôi
-                    childToResize.localScale = newScale;
-                }
+    //            if (childToResize != null) // Nếu child không null
+    //            {
+    //                Vector3 newScale = childToResize.localScale;
+    //                newScale.y *= 2; // Tăng kích thước y gấp đôi
+    //                childToResize.localScale = newScale;
+    //            }
                 
-            }
-        }
-    }
+    //        }
+    //    }
+    //}
 
-    private int CheckIndex()
-    {
-        for (int i = 0; i < listCurrentChild.Count; i++)
-        {
-            if (!listNewChild.Contains(listCurrentChild[i]))
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
+    //private int CheckIndex()
+    //{
+    //    for (int i = 0; i < listCurrentChild.Count; i++)
+    //    {
+    //        if (!listNewChild.Contains(listCurrentChild[i]))
+    //        {
+    //            return i;
+    //        }
+    //    }
+    //    return -1;
+    //}
 }
